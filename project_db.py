be773feee5bfd7ff952102a3fb2b352f7edba538
unknown <jetcraker@gmail.com>
2026-05-16 09:20:25 +0300

    reqs

--- project_db.py
+++ project_db.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
 
 
 engine = create_engine(
-    "postgresql+psycopg2://flask_database_hm8f_user:04wuX0sGZ6Y29Cp4nU6LuFIUPIRQV1AD@dpg-d840hi7aqgkc73a7fo70-a.oregon-postgres.render.com/flask_database_hm8f"
+    "postgresql+psycopg://flask_database_hm8f_user:04wuX0sGZ6Y29Cp4nU6LuFIUPIRQV1AD@dpg-d840hi7aqgkc73a7fo70-a.oregon-postgres.render.com/flask_database_hm8f"
 )
 Session = sessionmaker(bind=engine)
 
